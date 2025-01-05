18f83b66b5299662517ada9797005453d509ab4c
Abdulwaheed Abdulmujeeb <117639581+kingmuja12@users.noreply.github.com>
2025-01-04 21:16:04 -0330

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,1 +1,10 @@
+################## Prompt ################\
+from langchain_core.prompts import PromptTemplate
+prompt = PromptTemplate(
+    template="Tell me a {adjective} joke",
+    input_variables=["adjective"],
+)
 
+out_come = prompt.format(adjective="funny")
+
+print(out_come)
