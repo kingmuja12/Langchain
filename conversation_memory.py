1921f52bc561fdd1cbf3b1a36496a54fcf7ccfcf
Abdulwaheed Abdulmujeeb <117639581+kingmuja12@users.noreply.github.com>
2025-01-05 23:31:15 -0330

    Add files via upload

---
+++ conversation_memory.py
@@ -0,0 +1,14 @@
+################# Memory (using the type of conversation memory where by it can remember the amount of information you spicify) ##############
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_openai import OpenAI
+
+llm = OpenAI(temperature=0)
+memory = ConversationBufferWindowMemory(k=15) ### since k is equal to 15 then it can remember the last 15 conversation that we are having
+
+convo = ConversationChain(
+    llm=OpenAI(temperature=0.7),
+    memory=memory
+)
+convo.invoke("my name is Abdulmujeeb and i am the CTO of DYNAI")
+#print(convo.invoke("who is the CTO of DYNAI")["response"]) ### this prints out the response
