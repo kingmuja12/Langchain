16b3ad40a1cd7487cd74a7e87228e840d0e756b3
Abdulwaheed Abdulmujeeb <117639581+kingmuja12@users.noreply.github.com>
2025-01-06 23:13:37 -0330

    Update csv_access.py

--- csv_access.py
+++ csv_access.py
@@ -26,4 +26,4 @@ from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
 
-agent_executor.invoke({"input": "How many people have more than 3 siblings"})
+Input = agent_executor.invoke({"input": "How many people have more than 3 siblings"})
